6dda3353fb8cf14fff9488048b24e48e6d83f109
P-Correa <P-Correa@users.noreply.github.com>
2026-06-18 15:58:36 +0000

    deploy: ca2288eefffd0a3d3b9c3fa86f01ea3c6bc1679c

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2173159950"
+    serviceWorkerVersion: "3012580128"
   }
 });
